70f374721fc8e497abf35648985f4f337f88dc1b
Utku Ozgan <utku@successfactory.com>
2023-04-05 22:59:51 +0200

    heat: configure typescript for client

---
+++ client/src/App.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import App from "./App";
+
+test("renders learn react link", () => {
+  render(<App />);
+  const text = screen.getByText(/Here you go with Zoover/i);
+  expect(text).toBeInTheDocument();
+});
